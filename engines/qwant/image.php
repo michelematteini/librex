fbb9349cf9ed124bca85e3e33b611e41044585e4
Michele <Michele@PCMICHELE>
2024-12-25 01:03:15 +0100

    updated qwant image search  to display an error for known blocked code 27

--- engines/qwant/image.php
+++ engines/qwant/image.php
@@ -5,26 +5,31 @@
 
         $offset = $page * 5; // load 50 images per page
         
-        $url = "https://api.qwant.com/v3/search/images?q=$query&t=images&count=50&locale=en_us&offset=$offset&device=desktop&tgp=3&safesearch=1";
+        $url = "https://api.qwant.com/v3/search/images?q=$query&t=images&count=50&locale=en_us&offset=$offset&device=desktop&tgp=3&safesearch=0";
         $response = request($url);
         $json = json_decode($response, true);
         $results = array();
 
-        if ($json["status"] != "success")
-            return $results; // no results
-
-        $imgs = $json["data"]["result"]["items"];
-        $imgCount = $json["data"]["result"]["total"];
-
-        for ($i = 0; $i < $imgCount; $i++)
+        if ($json["status"] == "success")
         {
-            array_push($results, 
-                array (
-                    "thumbnail" => htmlspecialchars($imgs[$i]["thumbnail"]),
-                    "alt" => htmlspecialchars($imgs[$i]["title"]),
-                    "url" => htmlspecialchars($imgs[$i]["media"]) // use "url" for the source site url
-                )
-            );
+            $imgs = $json["data"]["result"]["items"];
+            $imgCount = $json["data"]["result"]["total"];
+
+            for ($i = 0; $i < $imgCount; $i++)
+            {
+                array_push($results, 
+                    array (
+                        "thumbnail" => htmlspecialchars($imgs[$i]["thumbnail"]),
+                        "alt" => htmlspecialchars($imgs[$i]["title"]),
+                        "url" => htmlspecialchars($imgs[$i]["media"]) // use "url" for the source site url
+                    )
+                );
+            }
+        }
+        else if ($json["status"] == "error")
+        {
+            if ($json["data"]["error_code"] == 27)
+                echo "<p>qwant blocked the request</p>";
         }
 
         return $results;
